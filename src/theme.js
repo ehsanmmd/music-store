10dcd1ba05b547e06c739c7000eefbf7d220774a
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-04-03 00:04:07 +0430

    Add ThemeProvider and farsi font faces

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+<link rel="stylesheet" href="index.css" />;
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Byekan"`,
+    fontSize: 14,
+  },
+});
+
+export default theme;
