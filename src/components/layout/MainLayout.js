829abaf8438e51daeef4577b5ad7f1fce5145b11
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-04-22 23:25:40 +0430

    update: add route for new product pages

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -3,8 +3,15 @@ import HomePage from "../../pages/HomePage";
 import MainContent from "./MainContent";
 import MainHeader from "./MainHeader";
 import PageFooter from "./PageFooter";
-import { Redirect, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProductPianos from "../../pages/products/ProductPianos";
+import ProductKeyboards from "../../pages/products/ProductKeyboards";
+import ProductClassicalGuitars from "../../pages/products/ProductBassGuitars";
+import ProductElectricGuitars from "../../pages/products/ProductElectricGuitars";
+import ProductBassGuitars from "../../pages/products/ProductBassGuitars";
+import ProductDrums from "../../pages/products/ProductDrums";
+import ProductViolins from "../../pages/products/ProductViolins";
+import ProductTrumpets from "../../pages/products/ProductTrumpets";
 
 const MainLayout = (props) => {
   return (
@@ -12,8 +19,21 @@ const MainLayout = (props) => {
       <MainHeader />
       <MainContent>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/product/pianos" element={<ProductPianos/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/piano" element={<ProductPianos />} />
+          <Route path="/products/keyboard" element={<ProductKeyboards />} />
+          <Route
+            path="/products/classical-guitar"
+            element={<ProductClassicalGuitars />}
+          />
+          <Route
+            path="/products/electric-guitar"
+            element={<ProductElectricGuitars />}
+          />
+          <Route path="/products/bass-guitar" element={<ProductBassGuitars />} />
+          <Route path="/products/violin" element={<ProductViolins />} />
+          <Route path="/products/drums" element={<ProductDrums />} />
+          <Route path="/products/trumpet" element={<ProductTrumpets />} />
         </Routes>
       </MainContent>
       <PageFooter />
