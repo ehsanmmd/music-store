255148d8492923b622cf47313624df3b530d6ca9
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-06-12 10:16:37 +0430

    update: Added text to the footer

--- src/components/layout/PageFooter.js
+++ src/components/layout/PageFooter.js
@@ -1,7 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 const PageFooter = (props) => {
-  return <Box sx={{ backgroundColor: grey[800], height: "10rem"}}></Box>;
+  return (
+    <Box sx={{ backgroundColor: grey[800], height: "10rem" }}>
+      <Grid container justifyContent="center" height="100%" alignItems="center">
+        <Typography color="#ffffff">
+        © تمامی حقوق این وب سایت برای شرکت موزیک استور محفوظ است.
+        </Typography>{" "}
+      </Grid>
+    </Box>
+  );
 };
 export default PageFooter;
